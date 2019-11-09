ab694876d1376dbe747d6e580b4ccbd37105980b
Laura Archambault <laarchambault@gmail.com>
2019-11-09 02:11:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,6 +21,8 @@ def get_japanese_emoticon(file_path, emoticon)
   answer
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  meaning_list = load_library(file_path)
+  if meaning_list[:get_meaning][emoticon]
+    answer = 
 end
