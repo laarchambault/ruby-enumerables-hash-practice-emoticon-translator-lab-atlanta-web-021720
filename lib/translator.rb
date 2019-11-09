0f6ca1def01927cf832628387cdb970850006efa
Laura Archambault <laarchambault@gmail.com>
2019-11-09 02:08:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  lookup_list = load_library(file_path)
+  meaning_list = load_library(file_path)
   meaning_list[:get_emoticon][emoticon]
 end
 
